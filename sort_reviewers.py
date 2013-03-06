112e2d7b24b063ea9439330c7f950ef51f5f5e54
John Dickinson <me@not.mn>
2013-03-06 12:34:53 -0800

    picked up prettytable formatting from https://gist.github.com/russellb/5082605

--- sort_reviewers.py
+++ sort_reviewers.py
@@ -2,6 +2,7 @@
 
 import json
 import sys
+import prettytable
 
 json_filename = sys.argv[1]
 
@@ -11,4 +12,12 @@ for reviewer, count in raw.items():
     if reviewer in 'smokestack jenkins':
         continue
     filtered.append((count, reviewer))
-print '\n'.join('%d %s' % x for x in sorted(filtered, reverse=True))
+filtered.sort(reverse=True)
+
+table = prettytable.PrettyTable(('Reviewer', 'Reviews'))
+total = 0
+for k, v in filtered:
+    table.add_row((v, k))
+    total += k
+print table
+print '\nTotal reviews: %d' % total
